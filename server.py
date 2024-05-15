d50b4e3712fe6ddd44fcc5e2fd984e5b4cd9ea8a
Asydrain <155194817+Asydrain@users.noreply.github.com>
2024-05-15 15:41:27 -0700

    Update server.py
    Correcting pylint findings

--- server.py
+++ server.py
@@ -44,6 +44,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(debug=True, host="0.0.0.0", port=5000)
